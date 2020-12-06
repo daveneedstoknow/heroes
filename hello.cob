90581d04a752b206f1639a7ac19f933d048b95b9
Dave Hounslow (Home) <daveh@dave-hounslow.remote.transficc.net>
2020-12-06 14:35:20 +0000

    *>     01 DETAILS.       *>             10 PIC X USING selectBox(1) LINE NUMBER PLUS 1 COL 1.       *>             10 pic 99 from hero-id(1)  col 3.       *>             10 pic X(20) from hero-name(1) COL 25.       *>             10 PIC X USING selectBox(2) LINE NUMBER PLUS 1 COL 1.       *>             10 pic 99 from hero-id(2) COL 3.       *>             10 pic X(20) from hero-name(2) COL 25.
          *>     01 SINGLE.
          *>         05 VALUE "Detail" LINE NUMBER PLUS 3 COL 1.
          *>         05 VALUE "ID #" LINE NUMBER PLUS 1 COL 1.
          *>         05 PIC 99 USING detailHeroId COL 10.
          *>         05 VALUE "NAME" LINE NUMBER PLUS 1 COL 1.
          *>         05 PIC X(20) USING detailHeroName COL 10.

--- hello.cob
+++ hello.cob
@@ -10,10 +10,15 @@
            01 keyStatus pic 9(4).
                88 PF-KEY-1 value 1001.
                88 PF-KEY-2 value 1002.
+               88 PF-KEY-3 value 1003.
+               88 PF-KEY-4 value 1004.
+               88 PF-KEY-8 value 1008.
+               88 PF-KEY-10 value 1010.
                88 PF-KEY-12 value 1012.
+          
            01 WS-ACCEPT-FNC-KEY PIC X.
           
-
+           01 heroCount pic 99 value 4.
            01 heroes.
                05 hero-values.
                    10 pic 99 value 01.
@@ -22,11 +27,13 @@
                    10 pic 99 value 02.
                    10 pic x(20) value "Batman".
 
+                   10 pic 99 value 11.
+                   10 pic x(20) value "Wonder Woman".
+
                    10 pic 99 value 03.
                    10 pic x(20) value "Spiderman".
+           
 
-                   10 PIC XX VALUE HIGH-VALUES.
-                   10 PIC X(20).
 
            01 hero-data redefines heroes.
                05 hero occurs 4 times.
@@ -42,59 +49,82 @@
            01 heroNumber PIC 99.
            01 lineNumber PIC 99.
 
+           01 selectedHeroNumber PIC 99 VALUE 1.
+           01 background-colour PIC 9.
+           01 col-highlight pic 9 value 4.
+           01 col-normal pic 9 value 0.
        SCREEN SECTION.
               
            01 HEADINGS.
+               
                05 VALUE "HEROES" col 20.
                05 VALUE "ID #" LINE NUMBER PLUS 3 COL 1.
                05 VALUE "NAME" COL 25.
+               
            
            01 DATA-ENTRY.
-               05 FILLER PIC X LINE NUMBER lineNumber TO WS-ACCEPT-FNC-KEY.
+               05 VALUE "F1 EXIT, F2 PREV, F3 NEXT" LINE NUMBER lineNumber COL 1.
+               05 FILLER PIC X TO WS-ACCEPT-FNC-KEY  LINE NUMBER PLUS 1 COL 1 .
 
+           01 SC-DASHBOARD-ROW.
+               10 hero-id PIC 99 COL 1 LINE NUMBER lineNumber
+                   BACKGROUND-COLOR background-colour. 
+               10 hero-name PIC X(20) COL 25 LINE NUMBER lineNumber
+                   BACKGROUND-COLOR background-colour.
 
-           01 SC-DETAILS.
-               10 hero-id PIC 99 COL 1 LINE NUMBER lineNumber.
-               10 hero-name PIC X(20) COL 25 LINE NUMBER lineNumber.
-
-      *>     01 DETAILS.
-      *>             10 PIC X USING selectBox(1) LINE NUMBER PLUS 1 COL 1.
-      *>             10 pic 99 from hero-id(1)  col 3.
-      *>             10 pic X(20) from hero-name(1) COL 25.
-      *>             10 PIC X USING selectBox(2) LINE NUMBER PLUS 1 COL 1.
-      *>             10 pic 99 from hero-id(2) COL 3.
-      *>             10 pic X(20) from hero-name(2) COL 25.
-
-      *>     01 SINGLE.  
-      *>         05 VALUE "Detail" LINE NUMBER PLUS 3 COL 1.
-      *>         05 VALUE "ID #" LINE NUMBER PLUS 1 COL 1.
-      *>         05 PIC 99 USING detailHeroId COL 10.
-      *>         05 VALUE "NAME" LINE NUMBER PLUS 1 COL 1.
-      *>         05 PIC X(20) USING detailHeroName COL 10.
-                       
+           01 SC-DETAILS-PANEL.
+               10  VALUE "My hero is " col 1 line number lineNumber.
+               10  hero-name PIC X(20).
               
        PROCEDURE DIVISION.
            DISPLAY HEADINGS.
 
 
-           PERFORM SHOW-DETAILS 
-                   VARYING heroNumber 
-                   FROM 1 BY 1
-                   UNTIL hero-id of hero(heroNumber) equal to HIGH-VALUES.
+           PERFORM display-it.
                    
            PERFORM accept-it UNTIL PF-KEY-1.
 
            STOP RUN.
 
-       SHOW-DETAILS.
+       display-it.
+           PERFORM SHOW-DASHBOARD-ROW 
+                   VARYING heroNumber 
+                   FROM 1 BY 1
+                   UNTIL heroNumber > heroCount.
+
+           PERFORM SHOW-DETAILS.
+
+       SHOW-DASHBOARD-ROW.
 
            ADD 5 to heroNumber GIVING lineNumber.
-           MOVE corresponding hero(heroNumber) to SC-DETAILS.
-           DISPLAY SC-DETAILS.
+           MOVE corresponding hero(heroNumber) to SC-DASHBOARD-ROW.
+           IF heroNumber IS equal selectedHeroNumber
+               MOVE col-highlight TO background-colour
+           else
+               MOVE col-normal TO background-colour.
+           DISPLAY SC-DASHBOARD-ROW.
+
+       SHOW-DETAILS.
+           add 2 to lineNumber.
+           MOVE corresponding hero(selectedHeroNumber) TO SC-DETAILS-PANEL.
+           DISPLAY SC-DETAILS-PANEL.
 
        accept-it.
+           
+           ADD 2 to lineNumber.
+           ACCEPT DATA-ENTRY .
+           evaluate TRUE
+               WHEN PF-KEY-3
+                   IF selectedHeroNumber < heroCount  
+                       ADD 1 TO selectedHeroNumber
+                   end-if
+               WHEN PF-KEY-2
+                   IF selectedHeroNumber > 1
+                       subtract 1 FROM selectedHeroNumber
+                   end-if
+           end-evaluate.
 
-           ADD 1 to lineNumber.    
-           ACCEPT DATA-ENTRY.
+           perform display-it. 
 
+           
            
